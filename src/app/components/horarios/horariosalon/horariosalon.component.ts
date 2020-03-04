10eb265a0593b2c822d634c4cdc9d711fcb48e33
rdpatino <37301737+rdpatino@users.noreply.github.com>
2020-03-04 12:13:41 -0500

    Horario - Sin terminar 3

--- src/app/components/horarios/horariosalon/horariosalon.component.ts
+++ src/app/components/horarios/horariosalon/horariosalon.component.ts
@@ -16,15 +16,15 @@ export interface PeriodicElement {
   viernes: string;
   sabado: string;
 }
-
+/* 
 const ELEMENT_DATA: PeriodicElement[] = [
   {hora: '7am', lunes: '', martes: '', miercoles: '', jueves: '', viernes: '', sabado: ''},
   {hora: '8am', lunes: '', martes: '', miercoles: '', jueves: '', viernes: '', sabado: ''},
-  {hora: '9am', lunes: '', martes: '', miercoles: '', jueves: '', viernes: '', sabado: ''},
+  {hora: '9am', lunes: 'Materia 1', martes: '', miercoles: '', jueves: 'Materia 1', viernes: '', sabado: ''},
   {hora: '10am', lunes: '', martes: '', miercoles: '', jueves: '', viernes: '', sabado: ''},
   {hora: '11am', lunes: '', martes: '', miercoles: '', jueves: '', viernes: '', sabado: ''},
 
-];
+];*/
 
 @Component({
   selector: 'app-horariosalon',
@@ -32,15 +32,21 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./horariosalon.component.css']
 })
 export class HorariosalonComponent implements OnInit {
+/* 
+  displayedColumns: string[] = ['hora', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado'];
+  dataSource = ELEMENT_DATA;
+  */
+  dataSource: PeriodicElement[];
+  displayedColumns: string[] ;
 
-horarios = null;
-  horario = {
-    hId: null,
-    hDia: null,
-    hHora: null,
-    hMateria: null,
-    hSalon: null,
-  };
+  horarios = null;
+    horario = {
+      hId: null,
+      hDia: null,
+      hHora: null,
+      hMateria: null,
+      hSalon: null,
+    };
 
   materiaNull: MateriaI = {
     matId: 0,
@@ -49,15 +55,23 @@ horarios = null;
     matPrograma: '',
     matCupo: 0,
     matSemestre: 0,
-    matProfesores: ''};
+    matProfesores: ''
+  };
 
-  displayedColumns: string[] = ['hora', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado'];
-  dataSource = ELEMENT_DATA;
+  horarioSalon2: MateriaI[] = [this.materiaNull, this.materiaNull, this.materiaNull, {matId: 1,
+    matNombre: 'materia 1',
+    matAsignacionTS: 'asignacionTS',
+    matPrograma: 'programa 1',
+    matCupo: 20,
+    matSemestre: 10,
+    matProfesores: 'profesores 1'}, this.materiaNull, this.materiaNull];
 
   constructor(private HorariossalonService: HorariossalonService) { }
 
   ngOnInit() {
     this.obtenerHorarios();
+    console.log('Cuantas veces hace el ngOnInit()');
+    
 
     let salones: string[] = ['Salon 1', 'Salon 2', 'Salon 3'];
     let dias: string[] = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado'];
@@ -65,33 +79,6 @@ horarios = null;
     let horarioSalon: (string[] | number[])[] = [salones , dias, horas];
     let horarioxSalon: MateriaI[] = [];
     //let horarioSalon1: MateriaI[] = [null, null, null, null, null, null];
-    let horarioSalon2: MateriaI[] = [this.materiaNull, this.materiaNull, this.materiaNull, {matId: 1,
-      matNombre: 'materia 1',
-      matAsignacionTS: 'asignacionTS',
-      matPrograma: 'programa 1',
-      matCupo: 20,
-      matSemestre: 10,
-      matProfesores: 'profesores 1'}, this.materiaNull, this.materiaNull];
-    //let horarioSalon3: MateriaI[] = [null, null, null, null, null, null];
-
-    const ELEMENT_DATA2: PeriodicElement[] = [
-      {hora: '7am', 
-      lunes: horarioSalon2[0].matNombre, 
-      martes: horarioSalon2[1].matNombre, 
-      miercoles: horarioSalon2[2].matNombre, 
-      jueves: horarioSalon2[3].matNombre, 
-      viernes: horarioSalon2[4].matNombre, 
-      sabado: horarioSalon2[5].matNombre},
-      {hora: '8am', lunes: '', martes: '', miercoles: '', jueves: '', viernes: '', sabado: ''},
-      {hora: '9am', lunes: '', martes: '', miercoles: '', jueves: '', viernes: '', sabado: ''},
-      {hora: '10am', lunes: '', martes: '', miercoles: '', jueves: '', viernes: '', sabado: ''},
-      {hora: '11am', lunes: '', martes: '', miercoles: '', jueves: '', viernes: '', sabado: ''},
-    
-    ];
-    let displayedColumns: string[] = ['hora', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado'];
-    let dataSource2 = ELEMENT_DATA2;
-
-    console.log(horarioSalon2);
 
     for (var salon of salones) {
       //console.log(salon);
@@ -105,21 +92,49 @@ horarios = null;
   }
 
 
-
   obtenerHorarios() {
     this.HorariossalonService.obtenerHorarios()
     .subscribe(
       result => this.horarios = result
     );
+    console.log('Obtiene Horarios');
   }
 
   hayRegistros() {
     if (this.horarios == null) {
+      //console.log('hayRegistros(): False');
       return false;
     } else {
+      //console.log('hayRegistros(): True');
+      //for (var horario of this.horarios) {
+        //console.log(horario[0]);
+      //}
       return true;
     }
   }
+
+  hayRegistrosHorarios() {
+    if (this.horarios == null) {
+      return false;
+    } else {
+        for (var horario of this.horarios) {
+          console.log(horario[0]);
+        }
+        this.dataSource = [
+          {hora: '7am', lunes: 'Materia 1', martes: '', miercoles: 'Materia1', jueves: '', viernes: '', sabado: ''},
+          {hora: '8am', lunes: '', martes: '', miercoles: '', jueves: '', viernes: '', sabado: ''},
+        ];
+        this.dataSource.push(
+          {hora: '9am', lunes: 'Materia 1', martes: '', miercoles: '', jueves: 'Materia 1', viernes: '', sabado: ''},
+        );
+        this.displayedColumns= ['hora', 'lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado'];
+        //this. dataSource = ELEMENT_DATA;
+      return true;
+    }
+  }
+
+  
+
 /*
   crearMateria() {
     console.log(this.horario);
